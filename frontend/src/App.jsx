96ef6e782477841258a2b3402f310321bfda97d1
Diego <165971972+dflow11@users.noreply.github.com>
2025-09-15 16:53:04 -0500

    added MovieCard component for displaying movie details, will be a movie displaying project.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import MovieCard from "./components/MovieCard";
 
 function App() {
   return (
-    <div>
-      <p>Hello world!</p>
-    </div>
+    <>
+      <MovieCard movie={{title: "Placeholder", release_date: "2025"}}/>
+    </>
   )
 }
 
-export default App
+export default App;
